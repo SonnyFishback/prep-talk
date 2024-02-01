1c4b0694be982c9b29ff597ea3fa8121e1d99315
SonnyFishback <sonny.fishback7@gmail.com>
2024-01-31 22:36:50 -0500

    created endpoint for submitting job description

---
+++ backend/src/functions/JobDescriptionSubmitted/JobDescriptionSubmitted.ts
@@ -0,0 +1,36 @@
+import {
+    APIGatewayProxyEventV2,
+    APIGatewayProxyHandlerV2,
+    APIGatewayProxyResultV2
+} from 'aws-lambda'
+
+export const handler: APIGatewayProxyHandlerV2 = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
+    try {
+        console.info('Job Description Submitted', JSON.stringify(event, null, 2));
+        const body = {
+            questions: [
+                {
+                    question: 'What is your name?'
+                },
+                {
+                    question: 'Tell me some things about yourself.'
+                },
+                {
+                    question: 'Tell me about a time you had to work with a difficult person. How did you handle it?'
+                }
+            ]
+        }
+        return {
+            statusCode: 200,
+            body: JSON.stringify(body),
+            isBase64Encoded: false
+        }
+    } catch (error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: 'An error occurred processing your request.',
+            isBase64Encoded: false
+        }
+    }
+}
