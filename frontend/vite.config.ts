4d9f6b1d87e2cbdde509476b0d70b84ea80fcb07
SonnyFishback <sonny.fishback7@gmail.com>
2024-01-31 23:05:09 -0500

    removed vite proxy now using server pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,14 +3,4 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	server: {
-		proxy: {
-		  "/api": {
-			target: "https://licf9v0cx2.execute-api.us-east-1.amazonaws.com",
-			changeOrigin: true,
-			secure: false,
-			rewrite: (path) => path.replace(/^\/api/, ""),
-		  },
-		},
-	  },
 });
