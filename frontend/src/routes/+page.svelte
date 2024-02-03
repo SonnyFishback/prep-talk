3fbcd4a23ff65c4635ca2f016a08ae6a01ce37fe
SonnyFishback <sonny.fishback7@gmail.com>
2024-02-03 18:59:35 -0500

    created interview route

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,42 +1,19 @@
+<svelte:head>
+    <title>Prep Talk</title>
+</svelte:head>
+
 <script lang='ts'>
-	import { enhance } from '$app/forms';
-    import type { ActionData } from './$types';
     import Button from '$lib/components/ui/button/button.svelte'
-    export let form: ActionData;
-    $:data = console.log(form);
-    const jobDescription = `
-        Job Title: Junior Software Engineer
-
-        Location: San Francisco, CA
-
-        Job Description:
-
-        We are looking for an enthusiastic Junior Software Engineer to join our development team. The ideal candidate will have a passion for technology and software development, with a willingness to learn and grow. You will work alongside experienced engineers to design, develop, and maintain software applications.
-
-        Responsibilities:
-
-        Assist in the design, development, and implementation of software applications.
-        Write clean, maintainable, and efficient code under the guidance of senior engineers.
-        Participate in code reviews to maintain code quality and share knowledge.
-        Collaborate with cross-functional teams to identify and resolve software issues.
-        Contribute to the development of technical documentation and user manuals.
-        Stay up-to-date with new technologies and programming best practices.
-        Qualifications:
-
-        Bachelor's degree in Computer Science, Engineering, or related field.
-        Knowledge of programming languages such as Java, C++, Python, or JavaScript.
-        Familiarity with software development methodologies like Agile and Scrum.
-        Basic understanding of databases and web development technologies.
-        Strong problem-solving and analytical skills.
-        Excellent communication skills and ability to work in a team environment.
-        Eagerness to learn and adapt to new challenges.
-    `
+    // import type { ActionData } from './$types';
+    // export let form: ActionData;
 </script>
 
-<main class='flex'>
-    <h2>Upload a job description to generate interview questions:</h2>
-    <form action='?/generate' method='post' use:enhance class='flex'>
-        <textarea name='description' id='description' cols='30' rows='10' value={jobDescription}/>
-        <Button type="submit">Submit</Button>
-    </form>
+<!-- <header class="min-h-[8vh]">
+</header> -->
+<main class="h-screen w-screen">
+    <section class="h-full bg-gradient-to-r from-[#283048] to-[#859398] flex flex-col justify-center items-center text-3xl">
+        <h2 class="text-white font-bold text-center mb-3">On-Demand personalized mock interviews</h2>
+        <p class="text-white text-center text-sm mb-5 px-6">Experience AI-driven interview coaching that guides you to your dream career.</p>
+        <Button variant="secondary" href="/interview">Start interview</Button>
+    </section>
 </main>
---
+++ frontend/src/routes/interview/+page.server.ts
@@ -0,0 +1,32 @@
+import { walk } from 'svelte/compiler';
+import type { PageServerLoad, Actions } from './$types';
+
+export const load = (async () => {
+    return {};
+}) satisfies PageServerLoad;
+
+export const actions = {
+    generate: async ({ request, fetch }) => {
+        try {
+            const form = await request.formData();
+            const description = form.get('description');
+            const url = 'https://b7oqgkz2ec.execute-api.us-east-1.amazonaws.com/generate';
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ description }),
+            }
+            const response = await fetch(url, options);
+            if (!response.ok) throw new Error('Uh Oh: Failed to generate questions.');
+            const data = await response.json();
+            const { questions } = data;
+            return {
+                ...questions
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+} satisfies Actions;
