bcaec270e9e6c86c3439604ba0beb48490dc7f2d
SonnyFishback <sonny.fishback7@gmail.com>
2024-02-06 21:30:13 -0500

    update sentry config

--- frontend/src/hooks.client.ts
+++ frontend/src/hooks.client.ts
@@ -4,6 +4,8 @@ import * as Sentry from '@sentry/sveltekit';
 Sentry.init({
   dsn: 'https://a0952dbb331523cb3f6636b29021fb84@o4506702422409216.ingest.sentry.io/4506702423719936',
   tracesSampleRate: 1.0,
+  debug: true,
+  environment: 'development'
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -5,6 +5,8 @@ import * as Sentry from '@sentry/sveltekit';
 Sentry.init({
   dsn: 'https://a0952dbb331523cb3f6636b29021fb84@o4506702422409216.ingest.sentry.io/4506702423719936',
   tracesSampleRate: 1.0,
+  debug: true,
+  environment: 'development'
 });
 
 // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
