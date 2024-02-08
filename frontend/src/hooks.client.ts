f16115670d4f3bed5537738a8aacd57641385de1
SonnyFishback <sonny.fishback7@gmail.com>
2024-02-07 19:12:51 -0500

    configured sentry to ingnor local

--- frontend/src/hooks.client.ts
+++ frontend/src/hooks.client.ts
@@ -1,11 +1,13 @@
 import { handleErrorWithSentry, replayIntegration } from "@sentry/sveltekit";
 import * as Sentry from '@sentry/sveltekit';
+import { dev } from '$app/environment';
 
 Sentry.init({
   dsn: 'https://a0952dbb331523cb3f6636b29021fb84@o4506702422409216.ingest.sentry.io/4506702423719936',
   tracesSampleRate: 1.0,
   debug: true,
-  environment: 'development',
+  environment: dev ? 'development' : 'production',
+  enabled: !dev,
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,12 +1,15 @@
 import { sequence } from "@sveltejs/kit/hooks";
 import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
 import * as Sentry from '@sentry/sveltekit';
+import { dev } from '$app/environment';
+
 
 Sentry.init({
   dsn: 'https://a0952dbb331523cb3f6636b29021fb84@o4506702422409216.ingest.sentry.io/4506702423719936',
   tracesSampleRate: 1.0,
   debug: true,
-  environment: 'development'
+  environment: dev ? 'development' : 'production',
+  enabled: !dev,
 });
 
 // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
