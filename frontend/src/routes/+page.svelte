db3feace1247b4249c7ee3b216cfc89c9eda9eb3
SonnyFishback <sonny.fishback7@gmail.com>
2024-02-17 19:21:29 -0500

    added gradient to landing page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="h-screen w-screen">
+    <section class="h-full bg-gradient-to-r from-[#283048] to-[#859398] flex flex-col justify-center items-center text-3xl">
+        <h2 class="text-white font-bold text-center mb-3">On-Demand personalized mock interviews</h2>
+        <p class="text-white text-center text-sm mb-5 px-6">Experience AI-driven interview coaching that guides you to your dream career.</p>
+    </section>
+</main>
